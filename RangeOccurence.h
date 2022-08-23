8f7e90f0688f5b5158d8d9d1cf31079db4255336
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-08-23 23:26:55 +0530

    Update RangeOccurence.h

--- RangeOccurence.h
+++ RangeOccurence.h
@@ -4,8 +4,7 @@ int rangeHighIndex;
 int rangeCount;
 }tdd_st;
 
-int checkRangeOccurence(int index, int currentSamples[], int *rangeLowIndex, int *rangeHighIndex, int *rangeCount);
-void skipSingleOccurence(int index, int currentSamples[], int *matchstatus, int *rangeLowIndex, int *rangeCount);
-void displayRangeOccurence(int index, int matchStatus, int currentSamples[], int array_size, int *rangeLowIndex, int *rangeHighIndex, int *rangeCount);
-void checkConsecutiveNumbers(int currentSamples[], int array_size);
+void checkNextConsecutiveNum(int currentSamples[], int *ptr_num, int *ptr_index, int position);
+void checkConsecutiveNum(int currentSamples[], int *ptr_num, int *ptr_index, int position);
+void checkRangeOccurence(int currentSamples[], int array_size);
 void DisplayConsecutiveNumRange(int currentSamples[], int array_size);
