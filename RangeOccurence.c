4ae37ec9d1e12a4c79293d198838d3ddc5dd218c
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-08-23 23:26:24 +0530

    Update RangeOccurence.c

--- RangeOccurence.c
+++ RangeOccurence.c
@@ -4,54 +4,40 @@
 #include "RangeOccurence.h"
 
 tdd_st rangeValues_st[20];
-int rangeNumIndex = 0;
 
-int checkRangeOccurence(int index, int currentSamples[], int *rangeLowIndex, int *rangeHighIndex, int *rangeCount){
-	int matchStatus = 0;
-	if((currentSamples[index] + 1 == currentSamples[index+1]) || (currentSamples[index] == currentSamples[index+1])){
-			*rangeHighIndex = currentSamples[index+1];
-			*rangeCount = *rangeCount+1;
-			matchStatus = 1;
-	}
-	return matchStatus;
-}
-
-void skipSingleOccurence(int index, int currentSamples[], int *matchstatus, int *rangeLowIndex, int *rangeCount){
-	if(*rangeCount == 1){
-		*matchstatus = 1;
-		*rangeLowIndex = currentSamples[index+1];
+void checkNextConsecutiveNum(int currentSamples[], int *ptr_num, int *ptr_index, int position){
+	if((*ptr_num + 1 < currentSamples[position])){
+		printf("\n Range %d - %d , %d", rangeValues_st[*ptr_index].rangeLowIndex, rangeValues_st[*ptr_index].rangeHighIndex, rangeValues_st[*ptr_index].rangeCount);
+		*ptr_index = *ptr_index +1;
+		rangeValues_st[*ptr_index].rangeLowIndex = currentSamples[position];
+		rangeValues_st[*ptr_index].rangeHighIndex = currentSamples[position];
+		rangeValues_st[*ptr_index].rangeCount = 1;
+		*ptr_num = currentSamples[position];
 	}
 }
 
-void displayRangeOccurence(int index, int matchStatus, int currentSamples[], int array_size, int *rangeLowIndex, int *rangeHighIndex, int *rangeCount){
-	skipSingleOccurence(index, currentSamples, &matchStatus, rangeLowIndex, rangeCount);
-	if((matchStatus != 1) || (index + 1 == array_size-1)){
-		rangeValues_st[rangeNumIndex].rangeLowIndex = *rangeLowIndex;
-		rangeValues_st[rangeNumIndex].rangeHighIndex = *rangeHighIndex;
-		rangeValues_st[rangeNumIndex].rangeCount = *rangeCount;
-		printf("\nRange %d - %d , %d", rangeValues_st[rangeNumIndex].rangeLowIndex,
-				rangeValues_st[rangeNumIndex].rangeHighIndex, rangeValues_st[rangeNumIndex].rangeCount);
-		*rangeLowIndex = currentSamples[index+1];
-		*rangeHighIndex = currentSamples[index+1];
-		*rangeCount = 1;
-		rangeNumIndex++;
+void checkConsecutiveNum(int currentSamples[], int *ptr_num, int *ptr_index, int position){
+	if((*ptr_num == currentSamples[position]) || (*ptr_num + 1 == currentSamples[position])){
+		rangeValues_st[*ptr_index].rangeCount++;
+		*ptr_num = currentSamples[position];
+		rangeValues_st[*ptr_index].rangeHighIndex = currentSamples[position];
 	}
 }
 
-void checkConsecutiveNumbers(int currentSamples[], int array_size){
-	int index = 0, rangeCount = 1, matchStatus = 0;
-	int rangeLowIndex = currentSamples[0];
-	int rangeHighIndex = currentSamples[0];
-	rangeNumIndex = 0;
+void checkRangeOccurence(int currentSamples[], int array_size){
+	int index = 0;
+	int num = rangeValues_st[index].rangeLowIndex = currentSamples[0];
+	rangeValues_st[index].rangeHighIndex = currentSamples[0];
+	rangeValues_st[index].rangeCount = 1;
 
-	while(index < array_size - 1){
-		matchStatus = checkRangeOccurence(index, currentSamples, &rangeLowIndex, &rangeHighIndex, &rangeCount);
-		displayRangeOccurence(index, matchStatus, currentSamples, array_size, &rangeLowIndex, &rangeHighIndex, &rangeCount);
-		index++;
+	for(int position=1; position<array_size; position++){
+		checkConsecutiveNum(currentSamples, &num, &index, position);
+		checkNextConsecutiveNum(currentSamples, &num, &index, position);
 	}
+	printf("\n Range %d - %d , %d", rangeValues_st[index].rangeLowIndex, rangeValues_st[index].rangeHighIndex, rangeValues_st[index].rangeCount);
 }
 
 void DisplayConsecutiveNumRange(int currentSamples[], int array_size){
 	sortCurrentSamples(currentSamples, array_size);
-	checkConsecutiveNumbers(currentSamples, array_size);
+	checkRangeOccurence(currentSamples, array_size);
 }
