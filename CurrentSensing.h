adf17f4a9994a7d056413ea05fb5153d1fe9ce00
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-08-29 23:06:15 +0530

    Update CurrentSensing.h

--- CurrentSensing.h
+++ CurrentSensing.h
@@ -1,6 +1,4 @@
-void ConversionForCurrentGreaterThanZero(int index, float adcValues[], float minRange, float maxRange, int currentValue[], int maxDigitalValue);
-
-void ConversionForCurrentLesserThanZero(int index, float adcValues[], float minRange, float maxRange, int currentValue[], int maxDigitalValue);
+void ConversionForCurrent(int index, float adcValues[], float minRange, float maxRange, int currentValue[], int maxDigitalValue);
 
 void checkMinRange(int index, float adcValues[], float minRange, float maxRange, int currentValue[], int maxDigitalValue);
 
